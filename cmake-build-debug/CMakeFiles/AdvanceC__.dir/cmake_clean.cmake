13e9676a60f8eddc259d7c807234811ad9b1879c
Ryan Lang <rlang5@unh.newhaven.edu>
2025-02-01 17:57:27 -0500

    - Deleted unnecessary "#include" statements - Got rid of extra spaces inbetween lines - Cleaned up over structure - Deleted random player class call in main

---
+++ cmake-build-debug/CMakeFiles/AdvanceC__.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "AdvanceC__"
+  "AdvanceC__.pdb"
+  "CMakeFiles/AdvanceC__.dir/Dice.cpp.o"
+  "CMakeFiles/AdvanceC__.dir/Dice.cpp.o.d"
+  "CMakeFiles/AdvanceC__.dir/Player.cpp.o"
+  "CMakeFiles/AdvanceC__.dir/Player.cpp.o.d"
+  "CMakeFiles/AdvanceC__.dir/enums.cpp.o"
+  "CMakeFiles/AdvanceC__.dir/enums.cpp.o.d"
+  "CMakeFiles/AdvanceC__.dir/main.cpp.o"
+  "CMakeFiles/AdvanceC__.dir/main.cpp.o.d"
+  "CMakeFiles/AdvanceC__.dir/tools.cpp.o"
+  "CMakeFiles/AdvanceC__.dir/tools.cpp.o.d"
+  "CMakeFiles/AdvanceC__.dir/unitTests.cpp.o"
+  "CMakeFiles/AdvanceC__.dir/unitTests.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AdvanceC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
