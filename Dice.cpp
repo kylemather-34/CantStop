ebf62b756a4b7c1bcc4d7c681bf58f520d065830
Kyle <matherkyle9@gmail.com>
2025-03-27 16:14:30 -0400

    P8 init, Game not implemented yet

--- Dice.cpp
+++ Dice.cpp
@@ -32,6 +32,77 @@ ostream& Dice::print(ostream& os) const {
     return os;
 }
 
+const int* CantStopDice::roll() {
+    const int* diceVals = Dice::roll();
+
+    cout << "Dice rolled: ";
+    for (int x = 0; x < 4; ++x) {
+        cout << char('A' + x) << ": " << diceVals[x] << " ";
+    }
+    cout << endl;
+
+    // Get player's choice (unchanged)
+    char pair1, pair2;
+    bool valid = false;
+    while (!valid) {
+        cout << "Choose first pair (e.g. AB): ";
+        cin >> pair1 >> pair2;
+        pair1 = toupper(pair1);
+        pair2 = toupper(pair2);
+
+        if (pair1 != pair2 && (pair1 >= 'A' && pair1 <= 'D')
+                          && (pair2 >= 'A' && pair2 <= 'D')) {
+            valid = true;
+                          } else {
+                              cout << "Invalid selection. Choose two different dice (A-D).\n";
+                          }
+    }
+    // Calculate pairs (using public getDice())
+    const int* vals = getDice();
+    pairValues[0] = vals[pair1-'A'] + vals[pair2-'A'];
+    pairValues[1] = vals[0] + vals[1] + vals[2] + vals[3] - pairValues[0];
+
+    return pairValues;
+}
+
+FakeDice::FakeDice() {
+    diceFile.open(FAKE_DICE_FILE);
+    if (!diceFile) {
+        fatal("Could not open fake dice file: " FAKE_DICE_FILE);
+    }
+}
+
+bool FakeDice::readNextRoll(int* values) {
+    string line;
+    if (!getline(diceFile, line)) return false;
+
+    istringstream iss(line);
+    for (int i = 0; i < 4; i++) {
+        if (!(iss >> values[i])) return false;
+    }
+    return true;
+}
+
+const int* FakeDice::roll() {
+    int values[4];
+    if (!readNextRoll(values)) {
+        fatal("End of fake dice file reached");
+    }
+
+    // Create a new array with the fake values
+    int* fakeRolls = new int[4];
+    for (int i = 0; i < 4; i++) {
+        fakeRolls[i] = values[i];
+    }
+
+    // Store in our pairValues array (inherited from CantStopDice)
+    myPairValues[0] = fakeRolls[0] + fakeRolls[1];
+    myPairValues[1] = fakeRolls[2] + fakeRolls[3];
+
+    delete[] fakeRolls;  // Clean up temporary array
+    return myPairValues;
+}
+
 //Output operator
 ostream& operator<<(ostream& os, const Dice& d) {
     return d.print(os);
--- Dice.hpp
+++ Dice.hpp
@@ -5,19 +5,40 @@
 #ifndef DICE_H
 #define DICE_H
 #include "tools.hpp"
-
+#define FAKE_DICE_FILE "fakedice.txt"
 
 class Dice {
-    private:
-        int nDice = 0; //# of dice in the set
-        int* rVal; // Array we will store rolls in
-    public:
-        Dice() : Dice(6) {} // Default to 6-sided dice
-        Dice(int n); // Constructing the class
-        ~Dice(); // Destructor
-        const int* roll(); // Function for "rolling" the dice
-        const int* getDice() const { return rVal; }
-        ostream& print(ostream&) const; // Print function
+private:
+    int nDice = 0; //# of dice in the set
+    int* rVal; // Array we will store rolls in
+public:
+    Dice() : Dice(6) {} // Default to 6-sided dice
+    Dice(int n); // Constructing the class
+    virtual ~Dice(); // Destructor
+    virtual const int* roll(); // Function for "rolling" the dice
+    const int* getDice() const { return rVal; }
+    ostream& print(ostream&) const; // Print function
 };
+
+class CantStopDice : Dice {
+private:
+    int pairValues[2];
+public:
+    CantStopDice() : Dice(4) {}
+    virtual ~CantStopDice() = default;
+    const int* roll() override;
+};
+
+class FakeDice : public CantStopDice {
+private:
+    ifstream diceFile;
+    int myPairValues[2];
+    bool readNextRoll(int* values);
+public:
+    FakeDice();
+    virtual ~FakeDice() {if (diceFile.is_open()) fatal("File could not be opened.");}
+    const int* roll() override;
+};
+
 ostream& operator<<(ostream&, const Dice&);
 #endif //DICE_H
